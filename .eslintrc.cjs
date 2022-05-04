bfedd40c1f7758344071f6d341cf3817737ecc00
Leon Si <contact@leonzalion.com>
2022-05-04 17:59:44 -0400

    chore: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,4 @@
-module.exports = {
-	extends: ['plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended'],
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		parser: '@typescript-eslint/parser',
-		ecmaVersion: 2018,
-		sourceType: 'module',
-		extraFileExtensions: ['.vue'],
-		project: './tsconfig.json'
-	},
-	rules: {
-		'@typescript-eslint/restrict-plus-operands': 'error'
-	}
-}
+const createESLintConfig = require('@leonzalion/configs/eslint.cjs');
+
+module.exports = createESLintConfig(__dirname);
+
